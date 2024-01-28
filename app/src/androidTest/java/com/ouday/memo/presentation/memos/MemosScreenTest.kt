dcd8038947041b1d1734a3585f7e01672ae184ee
Ouday <ouday.khaled@gmail.com>
2024-01-28 21:28:17 +0100

    The initial step in refactoring this suboptimal code is to establish an end-to-end (E2E) test. This ensures that subsequent refactoring efforts do not disrupt the application's functionality.

---
+++ app/src/androidTest/java/com/ouday/memo/presentation/memos/MemosScreenTest.kt
@@ -0,0 +1,58 @@
+package com.ouday.memo.memo.presentation.memos
+
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.ouday.memo.core.util.TestTags
+import com.ouday.memo.di.AppModule
+import com.ouday.memo.memo.presentation.MainActivity
+import com.ouday.memo.memo.presentation.util.Screen
+import com.ouday.memo.ui.theme.memoTheme
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.UninstallModules
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+@HiltAndroidTest
+@UninstallModules(AppModule::class)
+class MemosScreenTest {
+
+    @get:Rule(order = 0)
+    val hiltRule = HiltAndroidRule(this)
+
+    @get:Rule(order = 1)
+    val composeRule = createAndroidComposeRule<MainActivity>()
+
+    @ExperimentalAnimationApi
+    @Before
+    fun setUp() {
+        hiltRule.inject()
+        composeRule.setContent {
+            val navController = rememberNavController()
+            memoTheme {
+                NavHost(
+                    navController = navController,
+                    startDestination = Screen.MemosScreen.route
+                ) {
+                    composable(route = Screen.MemosScreen.route) {
+                        MemosScreen(navController = navController)
+                    }
+                }
+            }
+        }
+    }
+
+    @Test
+    fun clickToggleOrderSection_isVisible() {
+        composeRule.onNodeWithTag(TestTags.FILTER).assertDoesNotExist()
+        composeRule.onNodeWithContentDescription("Sort").performClick()
+    }
+}
